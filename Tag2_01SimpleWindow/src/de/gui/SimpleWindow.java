2164f371aae23b62bfee41106470d23b1ecadbb3
Jo Wagner <jowagner@limago.de>
2024-02-15 11:14:47 +0100

    initial commit

--- Tag2_01SimpleWindow/src/de/gui/SimpleWindow.java
+++ Tag2_01SimpleWindow/src/de/gui/SimpleWindow.java
@@ -3,14 +3,18 @@ package de.gui;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.Serializable;
 
 public class SimpleWindow extends Frame implements KeyListener {
 
+    private static final int SIZE = 500;
     private String message = "Hallo Fenster";
+    private int x,y;
 
     public SimpleWindow() {
+        x = y = SIZE/2;
         addKeyListener(this);// Briefträger Bescheid sagen
-        setSize(300,300);
+        setSize(SIZE,SIZE);
         setVisible(true);// Immer der letzte
     }
 
@@ -21,7 +25,7 @@ public class SimpleWindow extends Frame implements KeyListener {
     @Override
     public void paint(final Graphics g) {
         g.drawString(message, 50,50);
-
+        g.drawRect(x,y, 30,30);
     }
 
     @Override
@@ -31,6 +35,10 @@ public class SimpleWindow extends Frame implements KeyListener {
 
     @Override
     public void keyPressed(final KeyEvent e) { // Klingel anschrauben
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+        {
+            dispose();
+        }
         message = "Taste wurde gedrückt";
         repaint();
     }
