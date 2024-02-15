da39f921437a338f2ade7ef7a4c3e9b621fcc085
Jo Wagner <jowagner@limago.de>
2024-02-15 14:50:39 +0100

    initial commit

--- Tag2_03PaintClone/src/de/gothaer/paintclone/MyMenuBar.java
+++ Tag2_03PaintClone/src/de/gothaer/paintclone/MyMenuBar.java
@@ -1,16 +1,23 @@
 package de.gothaer.paintclone;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import static de.gothaer.paintclone.PaintClone.Form.*;
+import static java.awt.Color.*;
 
 public class MyMenuBar extends MenuBar {
 
-    public MyMenuBar(){
+    public MyMenuBar(final PaintClone paintClone){
         Menu menu;
         MenuItem menuItem;
 
         menu = new Menu("Datei");
 
         menuItem = new MenuItem("beenden");
+        menuItem.addActionListener(e->paintClone.dispose());
         menu.add(menuItem);
 
         add(menu);
@@ -18,14 +25,46 @@ public class MyMenuBar extends MenuBar {
         menu = new Menu("Form");
 
         menuItem = new MenuItem("Linie");
+        menuItem.addActionListener(e->paintClone.setForm(LINIE));
         menu.add(menuItem);
 
         menuItem = new MenuItem("Rechteck");
         menu.add(menuItem);
 
-        // Oval, Löschen
+        menuItem = new MenuItem("Oval");
+        menu.add(menuItem);
+
+        menu.addSeparator();
+
+        menuItem = new MenuItem("Löschen");
+        menu.add(menuItem);
+
+
         add(menu);
 
-        // Menü "Farben" schwarz, weiss, grün, rot, blau, gelb und pink
+        menu = new Menu("Farben");
+
+        menuItem = new MenuItem("Schwarz");
+        menuItem.addActionListener(e->paintClone.setFarbe(BLACK));
+        menu.add(menuItem);
+
+        menuItem = new MenuItem("Weiss");
+        menu.add(menuItem);
+        menuItem = new MenuItem("Rot");
+        menu.add(menuItem);
+
+        menuItem = new MenuItem("Grün");
+        menu.add(menuItem);
+
+        menuItem = new MenuItem("Blau");
+        menu.add(menuItem);
+
+        menuItem = new MenuItem("Gelb");
+        menu.add(menuItem);
+
+        menuItem = new MenuItem("Pink");
+        menu.add(menuItem);
+
+        add(menu);
     }
 }
--- Tag2_03PaintClone/src/de/gothaer/paintclone/PaintClone.java
+++ Tag2_03PaintClone/src/de/gothaer/paintclone/PaintClone.java
@@ -5,13 +5,40 @@ import java.awt.image.BufferedImage;
 
 public class PaintClone extends Frame {
 
+
+    public enum Form {LINIE,RECHTECK,OVAL,LOESCHEN};
     private static final int SIZE = 1000;
-    //private final BufferedImage image = new BufferedImage(SIZE,SIZE, BufferedImage.TYPE_INT_RGB);
+    private final BufferedImage image = new BufferedImage(SIZE,SIZE, BufferedImage.TYPE_INT_RGB);
+
+    private Form form = Form.LINIE;
+    private Color farbe = Color.WHITE;
+
+    public Form getForm() {
+        return form;
+    }
+
+    public void setForm(final Form form) {
+        this.form = form;
+    }
+
+    public Color getFarbe() {
+        return farbe;
+    }
+
+    public void setFarbe(final Color farbe) {
+        this.farbe = farbe;
+    }
 
     public PaintClone()  {
         super("PaintClone Version 1.0");
+        MyMenuBar menuBar = new MyMenuBar(this);
         setSize(SIZE,SIZE);
-        setMenuBar(new MyMenuBar());
+        setMenuBar(menuBar);
+    }
+
+    @Override
+    public void paint(final Graphics g) {
+        g.drawImage(image,0,0,this);
     }
 
     public static void main(String[] args) {
